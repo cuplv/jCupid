f42fa61d628a038c1f335de64a7fc09e676df5c9
Ian Martiny <ian.martiny@colorado.edu>
2016-02-18 15:21:17 -0700

    new example

---
+++ SumBytes.java
@@ -0,0 +1,42 @@
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Scanner;
+
+public class SumBytes
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.nextLine();
+
+        byte[] bArray = s.getBytes();
+
+        int sum = 0;
+        for(int i: bArray)
+            sum += i;
+
+        System.out.println("sum = " + sum);
+
+        MessageDigest md = null;
+
+        try
+        {
+            md = MessageDigest.getInstance("SHA-1");
+        }
+        catch(NoSuchAlgorithmException e)
+        {
+            System.out.println(e);
+        }
+
+        byte[] result;
+        if (sum < 500)
+        {
+            result = md.digest(bArray);
+        }
+        else
+        {
+            result = md.digest(md.digest(bArray));
+        }
+    }
+}
