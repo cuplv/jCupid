59e4e48015ff6d4e247f770fe536015398710f9d
Ian Martiny <ian.martiny@colorado.edu>
2016-05-05 16:49:17 -0600

    add/remove jCute instrumentation

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,13 +3,44 @@
  *
  * 
  */
+import java.util.Scanner;
 
 public class HelloWorld
 {
+
     public static void main(String [] args) 
     {
-        System.out.println("hello world");
+        
+        //Scanner sc = new Scanner(System.in);
+
+        //String s = sc.nextLine();
+
+        //if (s.length() < 5)
+        //{
+            //System.out.println("Small string");
+            //System.out.println("string is: " + s);
+        //}
+        //else if (s.length() < 10)
+        //{
+            //System.out.println("large string");
+            //System.out.println("string is: " + s);
+        //}
+        //else if (s.length() < 100)
+        //{
+            //System.out.println("very large string");
+            //System.out.println("string is " + s);
+        //}
+        //else
+        //{
+            //System.out.println("extremely large string");
+            //System.out.println("string is " + s);
+        //}
+        
+        System.out.println("Hello World");
+            
     }
 
 }
 
+
+//@jcute.optionJUnitOutputFolderName=/home/ian/Documents/bytecode-tool
