005a1bf8d485ac4638432becebf854631e4a41dc
Eric Wustrow <ewust@colorado.edu>
2016-05-24 19:02:55 -0600

    remove spurious &

--- Cmp.java
+++ Cmp.java
@@ -9,13 +9,20 @@ public class Cmp
 
     public static byte gt_constant(int a, int b)
     {
-        return (byte)(((((b - a) & (-1)) >> 31)&0x01)*0xFF);
+        return (byte)(((((b - a)) >> 31)&0x01)*0xFF);
     }
 
     public static void main(String[] args) {
         System.out.println(gt_constant(5, 8));
         System.out.println(gt_constant(8, 5));
         System.out.println(gt_constant(5000000, 8));
+        System.out.println(gt_constant(5000000, 500000000));
+        System.out.println(gt_constant(-1, -3));
+        System.out.println(gt_constant(-3, -1));
+        System.out.println(gt_constant(3, -8));
+        System.out.println(gt_constant(-8, 3));
+        System.out.println(gt_constant(3, 3));
+
 
 
     }
